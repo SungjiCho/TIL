c7abb7d63b7e31c09f31d9af861ad2f261a09875
SungjiCho <whtjdwl34@naver.com>
2020-04-05 19:27:28 +0900

    200405

---
+++ BaekJoon/src/입출력과사칙연산/AmultiplyB.java
@@ -0,0 +1,15 @@
+package 입출력과사칙연산;
+import java.io.IOException;
+
+public class AmultiplyB {
+
+	public static void main(String[] args) throws IOException{
+		int a = System.in.read();
+		System.in.read();
+		int b = System.in.read();
+		System.in.read();
+		a=a-48;
+		b=b-48;
+		System.out.println(a*b);		
+	}
+}
---
+++ BaekJoon/src/입출력과사칙연산/HelloWorld.java
@@ -0,0 +1,7 @@
+package 입출력과사칙연산;
+
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+}
