2fb9552b586f9bfda1e57ff09490daef0d0ffb29
SungjiCho <whtjdwl34@naver.com>
2020-04-10 22:22:38 +0900

    200410

--- BaekJoon/src/입출력과사칙연산/사칙연산.java
+++ BaekJoon/src/입출력과사칙연산/사칙연산.java
@@ -7,7 +7,6 @@ import java.util.StringTokenizer;
 public class 사칙연산 {
 	public static void main(String[] args) throws IOException{		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		String line = br.readLine();
 		StringTokenizer st = new StringTokenizer(line, " ");
 		int a = Integer.parseInt(st.nextToken());
