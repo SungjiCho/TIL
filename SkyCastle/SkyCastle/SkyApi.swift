8e215858ef3307d96077675e8cdcd86f3d85f1e1
SungjiCho <whtjdwl34@naver.com>
2020-05-21 16:40:15 +0900

    20200521

---
+++ SkyCastle/SkyCastle/SkyApi.swift
@@ -0,0 +1,9 @@
+//
+//  SkyApi.swift
+//  SkyCastle
+//
+//  Created by 조성지 on 2020/05/21.
+//  Copyright © 2020 조성지. All rights reserved.
+//
+
+import Foundation
---
+++ SkyCastle/SkyCastle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyCastle
+//
+//  Created by 조성지 on 2020/05/21.
+//  Copyright © 2020 조성지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
