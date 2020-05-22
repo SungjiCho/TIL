f6e4eda63b1d8d2e4813f0e37f1aa064b001fb21
SungjiCho <whtjdwl34@naver.com>
2020-05-23 08:41:41 +0900

    20200523

---
+++ HackerRank/Arrays/Arrays/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  Arrays
+//
+//  Created by 조성지 on 2020/05/23.
+//  Copyright © 2020 조성지. All rights reserved.
+//
+
+import Foundation
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!) else {
+   fatalError("Bad input")
+}
+
+guard let arrTemp = readLine() else { fatalError("Bad input") }
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let len = arr.count
+
+for i in 1...len{
+    print("\(arr[len - i]) ", terminator: "")
+}
+
+
