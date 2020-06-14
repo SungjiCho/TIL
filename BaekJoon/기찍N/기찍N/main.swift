292dc103e7991345d53664aeafcec8418c88d324
SungjiCho <whtjdwl34@naver.com>
2020-06-14 15:51:42 +0900

    20200614

---
+++ BaekJoon/기찍N/기찍N/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  기찍N
+//
+//  Created by 조성지 on 2020/06/14.
+//  Copyright © 2020 조성지. All rights reserved.
+//
+
+import Foundation
+
+guard let N = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!) else {
+    fatalError("Bad input")
+}
+
+for i in (1...N).reversed(){
+    print(i)
+}
+
