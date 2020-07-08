c74167d02673d52513c84c02aa116cb7ba2f6adc
SungjiCho <whtjdwl34@naver.com>
2020-07-08 16:53:29 +0900

    20200708

--- HackerRank/2DArrays/2DArrays/main.swift
+++ HackerRank/2DArrays/2DArrays/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let arr: [[Int]] = AnyIterator{ readLine() }.prefix(1).map {
+let arr: [[Int]] = AnyIterator{ readLine() }.prefix(6).map {
     let arrRow: [Int] = $0.split(separator: " ").map {
         if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
             return arrItem
@@ -16,9 +16,22 @@ let arr: [[Int]] = AnyIterator{ readLine() }.prefix(1).map {
     }
 
     guard arrRow.count == 6 else { fatalError("Bad input") }
-    print(arrRow)
 
     return arrRow
 }
 
-guard arr.count == 1 else { fatalError("Bad input") }
+guard arr.count == 6 else { fatalError("Bad input") }
+
+var max = [Int]()
+
+for i in 0..<4{
+    for j in 0..<4{
+        var sum = 0
+        sum += arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        sum += arr[i+1][j+1]
+        sum += arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+        max.append(sum)
+    }
+}
+
+print(max.max()!)
