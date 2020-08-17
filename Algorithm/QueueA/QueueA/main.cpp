53fee0cc7dc911f719655e651b9ef5bce88a0d52
SungjiCho <whtjdwl34@naver.com>
2020-08-18 06:21:04 +0900

    20200818

---
+++ Algorithm/QueueA/QueueA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QueueA
+//
+//  Created by 조성지 on 2020/08/16.
+//  Copyright © 2020 조성지. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
